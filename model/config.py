9d8de684cdfc6af96c3d200e9d9068a415a29dd2
Abhinav Gupta <abhinavgupta2004@gmail.com>
2020-03-21 02:16:48 -0400

    Adds config class

---
+++ model/config.py
@@ -0,0 +1,25 @@
+class Configuration(object):
+  def __init__(self, source, target):
+    self.src_data = source
+    self.trg_data = target
+    self.src_lang = 'de'
+    self.trg_lang = 'en'
+    self.epochs = 50
+    self.n_layers = 6
+    self.heads = 8
+    self.dropout = 0.1
+    self.printevery = 10
+    self.lr = 5e-4
+    self.emb_dim = 512
+    self.ff_hsize = 1024
+    self.max_strlen = 80
+    self.checkpoint = 0
+    self.device = 0
+    self.clip_norm = 0.0
+    self.src_pad = src.vocab.stoi['<pad>']
+    self.trg_pad = trg.vocab.stoi['<pad>']
+    self.k = 10
+    self.max_len = 80
+
+
+opt = Configuration(src, trg)
