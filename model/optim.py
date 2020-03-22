4a53d62c599872f2b97fb0b37f8233677db82e81
Anmol Jagetia <anmoljagetia@gmail.com>
2020-03-22 08:00:39 -0400

    Updates the optimizer code

--- model/optim.py
+++ model/optim.py
@@ -1,7 +1,8 @@
 from allennlp.training.learning_rate_schedulers.noam import NoamLR
 
-def get_std_opt(model, optimizer):
-    return NoamLR(optimizer,
+
+def get_std_opt(model, opt):
+    return NoamLR(opt.optimizer,
                   opt.emb_dim,
                   opt.warmup,
                   opt.factor)
